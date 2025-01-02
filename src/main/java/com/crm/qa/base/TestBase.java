3c10631899aa3170b4728417fae219fbca764100
NivruttiWagh <nivruttiwagh2002@gmail.com>
2025-01-02 13:40:49 +0530

    Added : Allure-Report Impl Code

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -17,41 +17,6 @@ import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
-//    public static WebDriver driver;
-//    public static Properties prop;
-//    public TestBase()
-//    {
-//        try{
-//            prop = new Properties();
-//            FileInputStream ip = new FileInputStream("C:\\java-workspace\\Selenium_Automation\\FreeCRMTestAutomation\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
-//            prop.load(ip);
-//        }
-//        catch (FileNotFoundException e)
-//        {
-//            e.printStackTrace();
-//        }
-//        catch (IOException e){
-//            e.printStackTrace();
-//        }
-//    }
-//    public static void initialization()
-//    {
-//        String browserName = prop.getProperty("browser");
-//        if(browserName.equals("chrome"))
-//        {
-//            WebDriverManager.chromedriver().setup();
-//            driver = new org.openqa.selenium.chrome.ChromeDriver();
-//        } else if (browserName.equals(("FF"))) {
-//            WebDriverManager.firefoxdriver().setup();
-//            driver = new org.openqa.selenium.firefox.FirefoxDriver();
-//        }
-//        driver.manage().window().maximize();
-//        driver.manage().deleteAllCookies();
-//        driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
-//        driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT,TimeUnit.SECONDS);
-//
-//        driver.get(prop.getProperty("url"));
-//    }
     public static Logger log = Logger.getLogger(TestBase.class);
     public static WebDriver driver;
     public static Properties prop;
